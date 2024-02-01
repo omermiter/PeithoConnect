ed21c8c760c62ec8b57a68b79279fbbcf24c2e65
Omer Mitran <omer3107@gmail.com>
2024-02-01 22:34:18 +0200

    added a tk window and fixed bugs

--- open_and_contact.py
+++ open_and_contact.py
@@ -29,7 +29,8 @@ def check_notification_message():
 
 
 #This function logs in to instagram with use's credentials
-def login_insta(user, message, index, counter):
+def login_insta(user, message, index, counter, users_arr, messages_arr, username, password):
+        
     if index == 0:
         try:
             elem = WebDriverWait(driver, 15).until(
@@ -40,25 +41,28 @@ def login_insta(user, message, index, counter):
             instagram_login_btn = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button')
 
 
-        instagram_username.send_keys("omermitran@gmail.com")
-        instagram_password.send_keys(",Hz2U#$zfDX$rwA")
+        instagram_username.send_keys(username)
+        instagram_password.send_keys(password)
         instagram_login_btn.click()
         time.sleep(12)
-        enter_messanger(user, message, counter, index)
+        enter_messanger(user, message, counter, index, users_arr, messages_arr)
     else:
-        enter_messanger(user, message, counter, index)
+        enter_messanger(user, message, counter, index, users_arr, messages_arr)
+
+            
+
         
     
     
 #Enters the messenger page
-def enter_messanger(user, message, counter, index):
+def enter_messanger(user, message, counter, index, users_arr, messages_arr):
     driver.get("https://www.instagram.com/direct/inbox/")
-    create_message(user, message, counter, index)
+    create_message(user, message, counter, index, users_arr, messages_arr)
 
 
 
 #Creates a new message
-def create_message(user, message, counter, index):
+def create_message(user, message, counter, index, users_arr, messages_arr):
     if (check_notification_message()):
         not_now = driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]')
         not_now.click()
@@ -70,7 +74,7 @@ def create_message(user, message, counter, index):
             for e in create_new_message_btn:
                 if e.text == "Send message":
                     e.click()
-                    search_user(user, message, counter, index)
+                    search_user(user, message, counter, index, users_arr, messages_arr)
     else:
         try:
             elem = WebDriverWait(driver, 15).until(
@@ -80,36 +84,42 @@ def create_message(user, message, counter, index):
             for e in create_new_message_btn:
                 if e.text == "Send message":
                     e.click()
-                    search_user(user, message, counter, index)
+                    search_user(user, message, counter, index, users_arr, messages_arr)
 
 
 
 #Searching a user 
-def search_user(user, message, counter,index):
+def search_user(user, message, counter,index, users_arr, messages_arr):
     try:
         WebDriverWait(driver, 5).until(
         EC.presence_of_element_located((By.XPATH, '/html/body/div[7]/div[1]/div/div[2]/div/div/div/div/div/div/div[1]/div/div[2]/div/div[2]/input')))
     finally:
         to_input = driver.find_element(By.NAME, 'queryBox')
         to_input.send_keys(user)
-        press_checkbox(message, counter, index)
+        press_checkbox(message, counter, index, users_arr, messages_arr)
+
+        
 
 
 
 #chooses the first option
-def press_checkbox(message, counter, index):
+def press_checkbox(message, counter, index, users_arr, messages_arr):
     try:
-        WebDriverWait(driver, 15).until(
-        EC.presence_of_element_located((By.NAME, 'ContactSearchResultCheckbox')))
-    finally:
-        user_checkboxes = driver.find_elements(By.NAME, 'ContactSearchResultCheckbox')
-        for checkbox in user_checkboxes:
-            checkbox.click()
-            break
-        start_chat(message, counter, index)
+        try:
+            WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.NAME, 'ContactSearchResultCheckbox')))
+        finally:
+            user_checkboxes = driver.find_elements(By.NAME, 'ContactSearchResultCheckbox')
+            for checkbox in user_checkboxes:
+                checkbox.click()
+                break
+            start_chat(message, counter, index, users_arr, messages_arr)
+    except NoSuchElementException:
+        index += 1
+        run_program(user= users_arr[index], message= messages_arr[index], index= index, counter= counter, users_arr= users_arr, messages_arr=messages_arr)
 
 #Choose the message text    
-def start_chat(message, counter, index):
+def start_chat(message, counter, index, users_arr, messages_arr):
     try:
         WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "div[role = 'button']")))
@@ -118,11 +128,11 @@ def start_chat(message, counter, index):
         for button in chat_btn:
             if button.text == "Chat":
                 button.click()
-                write_a_message(message, counter, index)
+                write_a_message(message, counter, index, users_arr, messages_arr)
 
 
 
-def write_a_message(message, counter, index):
+def write_a_message(message, counter, index, users_arr, messages_arr):
     try:
         WebDriverWait(driver, 15).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "div[role = 'textbox']")))
@@ -131,36 +141,37 @@ def write_a_message(message, counter, index):
         message_box.send_keys(message)
         message_box.send_keys(Keys.ENTER)
         time.sleep(10)
-        move_to_another_page(counter, index)
+        move_to_another_page(counter, index, users_arr, messages_arr)
         
 
-def move_to_another_page(counter, index):
+def move_to_another_page(counter, index, users_arr, messages_arr):
     urls_arr = ['https://www.instagram.com/', 'https://www.instagram.com/explore/', 'https://www.instagram.com/reels/']
     driver.get(random.choice(urls_arr))
     time.sleep(5)
-    if index != len(users_arr):
+    if index < len(users_arr):
         counter += 1
         index += 1
-        run_program(user= users_arr[index], message= messages_arr[index], index= index, counter= counter)
+        run_program(user= users_arr[index], message= messages_arr[index], index= index, counter= counter, users_arr= users_arr, messages_arr=messages_arr)
 
     
-def run_program(user, message, index, counter):
-    driver.get("https://www.instagram.com/")
-    login_insta(user, message, index, counter)
+def run_program(user, message, index, counter, users_arr, messages_arr, username, password):
+    if index < len(users_arr):
+        driver.get("https://www.instagram.com/")
+        login_insta(user, message, index, counter, users_arr, messages_arr, username, password)
 
 
-def read_csv_and_start():
-    index = 0
-    message_counter = 0
-    run_program(user= users_arr[index], message= messages_arr[index], index= index, counter = message_counter)
+def create_list_and_run(sheet, first_run, username, password):
+    cols = ["Users", "Messages"]
+    data = pd.read_csv(sheet, usecols=cols)
+    users_arr = data.Users.values.tolist()
+    messages_arr = data.Messages.values.tolist()
+    if(first_run):
+        index = 0
+        message_counter = 0
+        first_run = False
+    run_program(user= users_arr[index], message= messages_arr[index], index= index, counter = message_counter, users_arr= users_arr, messages_arr=messages_arr, username=username, password=password)
 
 
-cols = ["Users", "Messages"]
-data = pd.read_csv("Untitled spreadsheet - Sheet1.csv", usecols=cols)
-users_arr = data.Users.values.tolist()
-messages_arr = data.Messages.values.tolist()
-read_csv_and_start()
-
-
-
+first_run = True
+# create_list_and_run("Untitled spreadsheet - Sheet1.csv", first_run)
 
